2461c74e44523d57608aa87da715a56c662b526d
rlx <rlx@MacBook-Air.local>
2017-11-18 10:15:32 -0500

    added procmon

--- rlx/ml.py
+++ rlx/ml.py
@@ -3,6 +3,8 @@ import pandas as pd
 import rlx.utils as ru
 import matplotlib.pyplot as plt
 from joblib import delayed
+from pyprind import prog_bar as pbar
+w
 
 def abs_error(estimator, X, y):
     preds = estimator.predict(X)
@@ -21,9 +23,10 @@ class BaselinePredictor:
         return X[:, self.col]
 
     def score(self, X, y):
-        return abs_error(self, X,y)
+        return abs_error(self, X, y)
 
-def fit_score(estimator, X,y, fold_spec, scorer):
+
+def fit_score(estimator, X, y, fold_spec, scorer):
     from sklearn.base import clone
     from time import time
     e = clone(estimator)
@@ -72,7 +75,8 @@ def lcurve(estimator, X, y, scorer, cvs, n_jobs=-1, verbose=0):
     k = [{"cv":cv, "train_idxs": itr, "test_idxs":its} for cv in cvs for itr, its in cv.split(X,y)]
     r = ru.mParallel(n_jobs=n_jobs, verbose=verbose)(delayed(fit_score)(estimator, X, y, i, scorer) for i in k)
 
-    r = pd.DataFrame(r, columns=["cv", "estimator", "train_score", "test_score", "fit_time", "score_time"])
+    r = pd.DataFrame(r, columns=["cv", "estimator", "train_score",
+                                 "test_score", "fit_time", "score_time"])
 
     k = {str(i): i for i in r.cv}
     r = r.groupby([str(i) for i in r.cv]).agg([np.mean, np.std, len])
@@ -102,7 +106,6 @@ def plot_lcurve(rs):
         xlabels = np.r_[["%d" % i for i in range(len(nlabels))]]
         nlabels = np.arange(len(nlabels))
 
-
     idxs = np.argsort(-nlabels) if attr not in ["n_splits", "train_size"] else np.argsort(nlabels)
     tsm, tss = rs["test_score"]["mean"].values[idxs], rs["test_score"]["std"].values[idxs]
     trm, trs = rs["train_score"]["mean"].values[idxs], rs["train_score"]["std"].values[idxs]
@@ -110,7 +113,7 @@ def plot_lcurve(rs):
     plt.fill_between(range(len(tsm)), tsm-tss, tsm+tss, color="red", alpha=.1)
     plt.plot(trm, color="green", label="train", marker="o")
     plt.fill_between(range(len(trm)), trm-trs, trm+trs, color="green", alpha=.1)
-    plt.ylim(0,1.05)
+    plt.ylim(0, 1.05)
     plt.legend()
     plt.grid()
     plt.ylabel("score")
