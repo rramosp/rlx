0910f32c069bf7521cd820ed74e56e1e5db7d60e
rlx <rlx@MacBook-Air.local>
2017-11-24 14:02:26 -0500

    various

--- rlx/utils.py
+++ rlx/utils.py
@@ -317,3 +317,8 @@ def humanbytes(B):
         return '{0:.2f} GB'.format(B/GB)
     elif TB <= B:
         return '{0:.2f} TB'.format(B/TB)
+
+
+def pbar():
+    import progressbar
+    return progressbar.ProgressBar()
